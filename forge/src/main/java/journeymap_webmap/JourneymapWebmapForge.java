618e77b2c4554c8a6b2b67ce4366e6b313e1a0e3
Mysticdrew <drewhaas@gmail.com>
2026-08-25 16:15:11 -0500

    closes #19

--- forge/src/main/java/journeymap_webmap/JourneymapWebmapForge.java
+++ forge/src/main/java/journeymap_webmap/JourneymapWebmapForge.java
@@ -1,8 +1,18 @@
 package journeymap_webmap;
 
+import net.minecraftforge.event.GameShuttingDownEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
 public class JourneymapWebmapForge
 {
+    public JourneymapWebmapForge()
+    {
+        GameShuttingDownEvent.BUS.addListener(this::onGameShuttingDown);
+    }
+
+    private void onGameShuttingDown(GameShuttingDownEvent event)
+    {
+        WebMap.getInstance().stop();
+    }
 }

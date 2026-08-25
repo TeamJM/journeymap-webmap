e41ca4e05c1a27e4f698000d23b21a2110b14e60
Mysticdrew <drewhaas@gmail.com>
2026-08-25 16:15:11 -0500

    closes #19

--- fabric/src/main/java/journeymap_webmap/FabricClient.java
+++ fabric/src/main/java/journeymap_webmap/FabricClient.java
@@ -1,5 +1,13 @@
 package journeymap_webmap;
 
-public class FabricClient
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+
+public class FabricClient implements ClientModInitializer
 {
+    @Override
+    public void onInitializeClient()
+    {
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> WebMap.getInstance().stop());
+    }
 }
--- neoforge/src/main/java/journeymap_webmap/JourneymapWebmapNeoForge.java
+++ neoforge/src/main/java/journeymap_webmap/JourneymapWebmapNeoForge.java
@@ -2,8 +2,19 @@ package journeymap_webmap;
 
 
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.GameShuttingDownEvent;
 
 @Mod(Constants.MOD_ID)
 public class JourneymapWebmapNeoForge
 {
+    public JourneymapWebmapNeoForge()
+    {
+        NeoForge.EVENT_BUS.addListener(this::onGameShuttingDown);
+    }
+
+    private void onGameShuttingDown(GameShuttingDownEvent event)
+    {
+        WebMap.getInstance().stop();
+    }
 }

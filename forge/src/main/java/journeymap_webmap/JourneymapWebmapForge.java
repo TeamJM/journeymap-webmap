dd8e4e154655ccc02bbeba07549aac9864f9c587
Mysticdrew <drewhaas@gmail.com>
2026-08-25 16:50:03 -0500

    stupid forge

--- forge/src/main/java/journeymap_webmap/JourneymapWebmapForge.java
+++ forge/src/main/java/journeymap_webmap/JourneymapWebmapForge.java
@@ -2,13 +2,14 @@ package journeymap_webmap;
 
 import net.minecraftforge.event.GameShuttingDownEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class JourneymapWebmapForge
 {
     public JourneymapWebmapForge()
     {
-        GameShuttingDownEvent.BUS.addListener(this::onGameShuttingDown);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onGameShuttingDown);
     }
 
     private void onGameShuttingDown(GameShuttingDownEvent event)
